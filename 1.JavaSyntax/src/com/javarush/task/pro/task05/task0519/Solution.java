dbab85a16b0a883f464cd1cee5ee586b4e9c9c21
Yuliya-Bryksina <bakumovayv@gmail.com>
2023-01-11 15:36:29 +0300

    11.01

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0519/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0519/Solution.java
@@ -11,7 +11,16 @@ public class Solution {
     public static int[] array = {9, 8, 7, 6, 5, 4, 3, 2, 1};
     public static int element = 5;
 
+
     public static void main(String[] args) {
-        //напишите тут ваш код
+        int [] copyIndex = Arrays.copyOf(array, array.length);
+        Arrays.sort(copyIndex);
+       int index = Arrays.binarySearch(copyIndex, element);
+       if(index >= 0) {
+           System.out.println(true);
+       } else {
+           System.out.println(false);
+       }
+
     }
 }
