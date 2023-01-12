56b2398836b11da7b75bc47d82491b2a03ad0ad6
Yuliya-Bryksina <bakumovayv@gmail.com>
2023-01-12 17:10:28 +0300

    12.01

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0605/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.pro.task06.task0605;
+
+/* 
+Правильный порядок
+*/
+
+import java.util.Arrays;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int[] array = {11, 22, 33, 44, 55, 66, 77, 88, 99};
+        printArray(array);
+        reverseArray(array);
+        printArray(array);
+    }
+
+    public static void reverseArray(int[] array) {
+        for (int i = 0, y = array.length - 1; i <= array.length/2 && y >= array.length/2; i++,y--) {
+            int tempStart = array[i];
+            int tempEnd = array [y];
+            array[i] = tempEnd;
+            array[y] = tempStart;
+        }
+    }
+
+    public static void printArray(int[] array) {
+        for (int i : array) {
+            System.out.print(i + ", ");
+        }
+        System.out.println();
+    }
+}
