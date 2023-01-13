94d449d51c674a38ddbeae8ba2c3b33eed93999a
Yuliya-Bryksina <bakumovayv@gmail.com>
2023-01-13 14:12:47 +0300

    13.01

--- 1.JavaSyntax/src/com/javarush/task/pro/task06/task0608/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0608/Solution.java
@@ -6,8 +6,13 @@ package com.javarush.task.pro.task06.task0608;
 
 public class Solution {
     public static void main(String[] args) {
-
+        long a = 3;
+        cube(a);
     }
 
-    //напишите тут ваш код
+    public static long cube(long a) {
+        long b = a * a * a;
+        System.out.println(b);
+       return (b);
+    }
 }
