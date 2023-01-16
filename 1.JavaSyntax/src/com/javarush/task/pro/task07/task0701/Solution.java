4f2343b91b959ce8b7aaae65b78f6c7427a0f7f1
Yuliya-Bryksina <bakumovayv@gmail.com>
2023-01-16 18:17:39 +0300

    16.01

--- 1.JavaSyntax/src/com/javarush/task/pro/task07/task0701/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0701/Solution.java
@@ -5,5 +5,12 @@ package com.javarush.task.pro.task07.task0701;
 */
 
 public class Solution {
-    //напишите тут ваш код
+    int a;
+    long b;
+    byte c;
+    short g;
+    float aa;
+    double bb;
+    boolean cc;
+    char gg;
 }
