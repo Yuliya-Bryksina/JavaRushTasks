8a39d2ebc3ed57fe1f593d20d33f1007e28dc8e4
Yuliya-Bryksina <bakumovayv@gmail.com>
2023-02-17 17:13:39 +0300

    15.02

--- 1.JavaSyntax/src/com/javarush/task/pro/task10/task1006/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1006/Solution.java
@@ -6,6 +6,33 @@ package com.javarush.task.pro.task10.task1006;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        HybridCar hybridCar = new HybridCar();
+        GasCar gasCar = new GasCar();
+        ElectricCar electricCar = new ElectricCar();
+    }
+
+    public static class Car {
+
+        public Car(String value) {
+            System.out.println(value);
+        }
+    }
+
+    public static class ElectricCar extends Car {
+        public ElectricCar() {
+            super("Привет. Я ElectricCar");
+        }
+    }
+
+    public static class GasCar extends Car {
+        public GasCar() {
+            super("Привет. Я GasCar");
+        }
+    }
+
+    public static class HybridCar extends Car {
+        public HybridCar() {
+            super("Привет. Я HybridCar");
+        }
     }
 }
