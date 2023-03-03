4c2a3a852e5ac33888cb226918dd218092a2e6c0
Yuliya-Bryksina <bakumovayv@gmail.com>
2023-03-03 17:10:17 +0300

    03.03

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task11/task1104/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task11.task1104;
+
+/* 
+Конвертер строки
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String string = "12.84";
+        double result = parseDouble(string);
+        System.out.println(Math.round(result));
+    }
+
+    public static double parseDouble(String string){
+        double convertToDouble = Double.parseDouble(string);
+        return convertToDouble;
+    }
+}
